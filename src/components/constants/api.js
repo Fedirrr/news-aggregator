9ed2c01f67c35e8a75fa8e9c8048300cd2da7080
fedyapavlyshynets <fedyapavlyshynets@gmail.com>
2024-10-04 22:04:23 +0200

    updated apis

--- src/components/constants/api.js
+++ src/components/constants/api.js
@@ -19,7 +19,7 @@ export const path = {
     }
 }
 
-export const newsApiKey = "16a87a9dc0794997a589c6bf540fa923";
 export const NYTApiKey = "k6snHqyQxfZ81rGL6H7tKLdgZumwj9QX";
+export const newsApiKey = "16a87a9dc0794997a589c6bf540fa923";
 export const guardianApiKey = "ca115ca2-d563-417d-b2de-7d290400a54d";
 // All api keys will be moved to a .env file in a real project
